a2048ad11e25be8e0155ffb821393e5172f80176
alonecandies <dragonfrezze@gmail.com>
2022-03-12 02:40:12 +0700

    Object

---
+++ Object.ts
@@ -0,0 +1,17 @@
+export {};
+// Object
+let userData: { name: string; age: number } = {
+  name: "John",
+  age: 30,
+};
+// userData.name = 20;
+// userData.age = "30";
+// userData.test = "Book";
+// Cannot modify a false type to property of object
+
+// userData = {
+//    name: "Anna",
+//    age: 20,
+//    test: "Book"
+// }
+// Cannot assign new value to false type
