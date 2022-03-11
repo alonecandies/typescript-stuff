45203ce8cc8bfc4eefead81a040d3301629ea921
alonecandies <dragonfrezze@gmail.com>
2022-03-12 02:39:40 +0700

    Array

---
+++ Array.ts
@@ -0,0 +1,12 @@
+export {};
+// Array
+let names = ["John", "Jane", "Mary"];
+names.push("Anna");
+// names.push(5);
+// names.push(false)
+// Cannot push a boolean to an string array
+
+let mixed = [1, "test", false];
+mixed.push(true);
+// mixed.push({})
+// Cannot push a object to an mixed array
