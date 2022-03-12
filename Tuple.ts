07e4e3f0421cd4889bdfae7ebfebd05a95e634af
alonecandies <dragonfrezze@gmail.com>
2022-03-12 19:05:57 +0700

    Tuple

---
+++ Tuple.ts
@@ -0,0 +1,11 @@
+// Tuple
+// let arr = ["long", 30, true];
+// arr[0] = 20;
+// arr[1] = "longer";
+// arr[2] = 30;
+// console.log(arr);
+
+const tup: [string, number, boolean] = ["long", 30, true];
+// tup[0] = 30;
+// tup[1] = "longer";
+// tup[2] = 30;
