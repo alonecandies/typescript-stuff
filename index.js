058e3db78eb241318262e363cf923b61090de2e8
alonecandies <dragonfrezze@gmail.com>
2022-03-12 19:06:48 +0700

    Henry Web Dev

---
+++ index.js
@@ -0,0 +1,23 @@
+var resourceOne = {
+    id: 1,
+    name: "Resource One",
+    data: "Some data" //Must be string
+};
+var resourceTwo = {
+    id: 2,
+    name: "Resource Two",
+    data: {
+        name: "John Doe",
+        age: 30
+    }
+};
+var resourceThree = {
+    id: 3,
+    name: "Resource Three",
+    data: ["Some", "Data", "Array"]
+};
+var numbersResource = {
+    id: 4,
+    name: "Resource Four",
+    data: [1, 2, 3, 4, 5]
+};
